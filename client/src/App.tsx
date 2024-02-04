dddd501b3f6be3919897c00a9c7f2c96a7ea2360
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-02-04 12:28:09 +0300

    Typical header, main and footer sections in App's div

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div></div>
+    <div>
+      <header></header>
+
+      <main></main>
+
+      <footer></footer>
+    </div>
   )
 }
 
