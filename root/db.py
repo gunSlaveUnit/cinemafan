fb1e96a5d0152be69da3530377af3d30bc190338
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-05 22:41:38 +0300

    DB session generator

--- root/db.py
+++ root/db.py
@@ -67,3 +67,18 @@ session_maker: async_sessionmaker = async_sessionmaker(
     expire_on_commit=False,
     autoflush=False,
 )
+
+
+async def session() -> AsyncGenerator[AsyncSession, None]:
+    """
+    Provides a database session.
+
+    Returns:
+        AsyncGenerator[AsyncSession, None]: database session.
+    """
+
+    async with session_maker() as s:
+        try:
+            yield s
+        finally:
+            await s.close()
