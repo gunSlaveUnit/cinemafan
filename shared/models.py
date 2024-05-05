3167b20039cae1b11fc5f8be6d3a0e4b067018f1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-05 22:32:05 +0300

    Entity model

---
+++ shared/models.py
@@ -0,0 +1,23 @@
+import datetime
+
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from ..root.models import Base
+
+
+class Entity(Base):
+    """
+    Used as base class for all models
+    with useful base fields
+    like id, created_at, updated_at etc.
+    """
+
+    __abstract__ = True
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(onupdate=func.now())
