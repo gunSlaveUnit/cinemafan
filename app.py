3a0531929cb9ff3e68ee396241e053d005c68e51
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-07 23:47:52 +0300

    Index route included to app

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+
+from root.templates import templates
 
 app = FastAPI(
     title="cinemafan",
 )
+
+
+@app.get("/")
+async def index(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+    )
