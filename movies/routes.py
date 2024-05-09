9e2dd4baaccefacb1bbf433c85c6b6d013ff2f0e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-09 18:48:45 +0300

    Movies router separated into api_router and templates_router

--- movies/routes.py
+++ movies/routes.py
@@ -2,10 +2,11 @@ from fastapi import APIRouter, Request
 
 from root.templates import templates
 
-router = APIRouter(prefix="/movies", tags=["Movies"])
+api_router = APIRouter(prefix="/api/movies", tags=["Movies", "API"])
+templates_router = APIRouter(prefix="/movies", tags=["Movies", "Templates"])
 
 
-@router.get("")
+@templates_router.get("")
 async def items(request: Request):
     return templates.TemplateResponse(
         request=request,
