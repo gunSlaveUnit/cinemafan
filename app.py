87068197d4873b51cdecf4e66fb133efe22b9551
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-09 18:44:01 +0300

    Used init db in lifespan

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
+from contextlib import asynccontextmanager
+from typing import AsyncGenerator
+
 from fastapi import FastAPI, Request
 
 from movies.routes import router as movies_router
+from root.db import init
 from root.templates import templates
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI) -> AsyncGenerator:
+    await init()
+
+    yield
+
 
 app = FastAPI(
     title="cinemafan",
+    lifespan=lifespan,
 )
 
 
@@ -14,5 +27,6 @@ async def index(request: Request):
         request=request,
         name="index.html",
     )
+
 
 app.include_router(movies_router)
