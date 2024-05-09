879cede23cdc9e728e68b8b990bcf1908ea87b47
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-09 19:07:17 +0300

    MovieDBSchema created

--- movies/schemas.py
+++ movies/schemas.py
@@ -1,5 +1,11 @@
 from pydantic import BaseModel
 
+from shared.schemas import EntityDBSchema
+
 
 class MovieCreateSchema(BaseModel):
     title: str
+
+
+class MovieDBSchema(MovieCreateSchema, EntityDBSchema):
+    pass
