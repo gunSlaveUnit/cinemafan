f31ff6092f0f273f7679a50f57b81d2fb5203165
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-09 18:52:34 +0300

    Items api route, which also used in movies templates route

--- movies/routes.py
+++ movies/routes.py
@@ -6,9 +6,28 @@ api_router = APIRouter(prefix="/api/movies", tags=["Movies", "API"])
 templates_router = APIRouter(prefix="/movies", tags=["Movies", "Templates"])
 
 
+@api_router.get("")
+async def items():
+    return {
+        "data": [
+            {
+                "title": "Test movie 1",
+            },
+            {
+                "title": "Test movie 2",
+            }
+        ]
+    }
+
+
 @templates_router.get("")
-async def items(request: Request):
+async def movies(request: Request):
+    content = await items()
+
     return templates.TemplateResponse(
         request=request,
         name="movies/movies.html",
+        context={
+            "movies": content["data"],
+        }
     )
