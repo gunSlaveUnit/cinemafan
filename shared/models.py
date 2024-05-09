b77a075c61cbbffcf900e1126cec1f9934ab1bb6
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-09 19:06:40 +0300

    Fixed updated_at field in Entity model

--- shared/models.py
+++ shared/models.py
@@ -16,8 +16,5 @@ class Entity(Base):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    created_at: Mapped[datetime.datetime] = mapped_column(
-        server_default=func.now(),
-        nullable=False,
-    )
-    updated_at: Mapped[datetime.datetime] = mapped_column(onupdate=func.now())
+    created_at: Mapped[datetime.datetime] = mapped_column(server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(server_default=None, onupdate=func.now(), nullable=True)
