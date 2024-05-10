c970e63c20b16b9760f041db5d047cc90994a33e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-10 17:05:14 +0300

    Fixed used model in episodes api

--- movies/api/episodes.py
+++ movies/api/episodes.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from movies.models import Quality
+from movies.models import Quality, Episode
 from movies.schemas import EpisodeCreateSchema
 from root.db import session
 
@@ -10,7 +10,7 @@ router = APIRouter(prefix="/episodes", tags=["Episodes"])
 
 @router.get("")
 async def items(db: AsyncSession = Depends(session)):
-    data = [_ async for _ in Quality.every(db)]
+    data = [_ async for _ in Episode.every(db)]
 
     return {
         "data": data,
@@ -20,9 +20,9 @@ async def items(db: AsyncSession = Depends(session)):
 
 @router.get("/{item_id}")
 async def item(item_id: int, db: AsyncSession = Depends(session)):
-    return await Quality.by_id(item_id, db)
+    return await Episode.by_id(item_id, db)
 
 
 @router.post("")
 async def create(data: EpisodeCreateSchema, db: AsyncSession = Depends(session)):
-    return await Quality.create(data.model_dump(), db)
+    return await Episode.create(data.model_dump(), db)
