494418a2a26153cf9b0bc41024638ddcbe5493ed
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-10 10:52:08 +0300

    Episode model

--- movies/models.py
+++ movies/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 
 from shared.models import Entity
@@ -7,4 +7,14 @@ from shared.models import Entity
 class Movie(Entity):
     __tablename__ = "movies"
 
+    title: Mapped[str] = mapped_column(String(255))
+
+
+class Episode(Entity):
+    __tablename__ = "episodes"
+
+    movie_id: Mapped[int] = mapped_column()
+    number: Mapped[int] = mapped_column()
+    parent_id: Mapped[int] = mapped_column(ForeignKey("episodes.id"))
+    season: Mapped[int] = mapped_column()
     title: Mapped[str] = mapped_column(String(255))
