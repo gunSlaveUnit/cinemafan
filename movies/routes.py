c12f9aa661d86efc3b71391c533bd0ed176b988e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-10 17:06:16 +0300

    Bound episode templates router

--- movies/routes.py
+++ movies/routes.py
@@ -4,6 +4,7 @@ from movies.api.episodes import router as episodes_api_router
 from movies.api.movies import router as movies_api_router
 from movies.api.qualities import router as qualities_api_router
 from movies.api.records import router as records_api_router
+from movies.templates.episodes import router as episodes_templates_router
 from movies.templates.movies import router as movies_templates_router
 
 api_router = APIRouter(prefix="/api", tags=["API"])
@@ -13,6 +14,7 @@ api_router.include_router(qualities_api_router)
 api_router.include_router(records_api_router)
 
 templates_router = APIRouter(prefix="", tags=["Templates"])
+templates_router.include_router(episodes_templates_router)
 templates_router.include_router(movies_templates_router)
 
 router = APIRouter()
