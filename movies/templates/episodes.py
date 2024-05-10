c5bb14adb676ee13dce648aa187317c38ec42fdb
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-10 17:14:29 +0300

    Pass records to episode page

--- movies/templates/episodes.py
+++ movies/templates/episodes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Request, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from movies.api import episodes
+from movies.models import Record
 from root.db import session
 from root.settings import templates
 
@@ -15,11 +16,13 @@ async def item(
         db: AsyncSession = Depends(session)
 ):
     response = await episodes.item(item_id, db)
+    records = [_ async for _ in Record.by_episode_id(item_id, db)]
 
     return templates.TemplateResponse(
         request=request,
         name="movies/episode.html",
         context={
             "episode": response,
+            "records": records,
         }
     )
