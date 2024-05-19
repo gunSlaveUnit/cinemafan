cede240f1654ef4a9d12c5e092dcef5ddc5d4e40
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-19 19:35:35 +0300

    Created examples package with init script and data files

---
+++ examples/__init__.py
@@ -0,0 +1,8 @@
+"""
+This package is not a part of the main project.
+
+The script and data file in this package
+are intended only for quickly filling the database with test data.
+
+Periodically modified to suit the current data schema.
+"""
