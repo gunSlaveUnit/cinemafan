ce137a9333de461386da0ac4c1c49bd8219f7790
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-05-22 23:59:11 +0300

    StudioCreateSchema

--- movies/schemas.py
+++ movies/schemas.py
@@ -5,6 +5,10 @@ from pydantic import BaseModel
 from shared.schemas import EntityDBSchema
 
 
+class StudioCreateSchema(BaseModel):
+    title: str
+
+
 class PersonCreateSchema(BaseModel):
     name: str
 
