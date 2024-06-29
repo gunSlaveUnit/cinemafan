b852f586bd3eef51bc5fde905f74f900371d8fdb
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-06-29 18:40:44 +0300

    Index route in root/routes, removed templates folder in root

--- root/routes.py
+++ root/routes.py
@@ -1,9 +1,31 @@
-from fastapi import APIRouter
+import random
 
-from root.templates.index import router as index_router
+from fastapi import APIRouter, Request, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
 
-templates_router = APIRouter(prefix="", tags=["Templates"])
-templates_router.include_router(index_router)
+from infrastructure.db import get_db
+from infrastructure.settings import templates
+from movies.api import movies
 
-router = APIRouter()
-router.include_router(templates_router)
+router = APIRouter(prefix="", tags=["root"])
+
+
+@router.get("/")
+async def index(
+        request: Request,
+        db: AsyncSession = Depends(get_db),
+):
+    response = await movies.items(db)
+    data = response["data"]
+
+    context = {}
+    movie_id = None
+    if data:
+        movie_id = random.choice(data).id
+    context["movie_id"] = movie_id
+
+    return templates.TemplateResponse(
+        request=request,
+        name="root/index.html",
+        context=context,
+    )
