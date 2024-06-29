1193841177434a4e06a8c03a3029884e77d6141f
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-06-29 17:05:06 +0300

    No comments

--- root/models.py
+++ root/models.py
@@ -3,9 +3,4 @@ from sqlalchemy.orm import DeclarativeBase
 
 
 class Base(DeclarativeBase, AsyncAttrs):
-    """
-    Base class to work with SQLAlchemy functionality.
-    Primarily used for Entity class.
-    """
-
     pass
