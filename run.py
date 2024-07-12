a9a1335dd00cd968e8a3551227c1e30524f83bde
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-07-12 22:57:53 +0300

    Setup host, port and reload from .env

--- run.py
+++ run.py
@@ -1,7 +1,19 @@
-import uvicorn
+import os
 
 from dotenv import load_dotenv
+import uvicorn
+
 load_dotenv()
 
 if __name__ == '__main__':
-    uvicorn.run("app:app", reload=True)
+    HOST = os.getenv("HOST")
+    PORT = int(os.getenv("PORT"))
+    RELOAD = os.getenv("RELOAD") == "True"
+    WORKERS = 1 if RELOAD else os.cpu_count() + 1
+
+    uvicorn.run("app:app",
+        host=HOST,
+        port=PORT,
+        reload=RELOAD,
+        workers=WORKERS,
+    )
