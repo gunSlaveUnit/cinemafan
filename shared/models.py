b729de50b367edc1a1e88818ff1cd30b06f4a2af
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-07-15 19:46:01 +0300

    Useful methods comeback in Entity model

--- shared/models.py
+++ shared/models.py
@@ -1,4 +1,7 @@
-from sqlalchemy.ext.asyncio import AsyncAttrs
+from typing import AsyncIterator
+
+from sqlalchemy import func, select
+from sqlalchemy.ext.asyncio import AsyncAttrs, AsyncSession
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
@@ -10,3 +13,31 @@ class Entity(Base):
     __abstract__ = True
 
     id: Mapped[int] = mapped_column(primary_key=True)
+
+    @classmethod
+    async def every(cls, session: AsyncSession) -> AsyncIterator:
+        scalars = await session.stream_scalars(select(cls))
+        async for scalar in scalars:
+            yield scalar
+
+    @classmethod
+    async def by_id(
+            cls,
+            item_id: int,
+            session: AsyncSession,
+    ):
+        return await session.scalar(select(cls).where(cls.id == item_id))
+
+    @classmethod
+    async def create(
+            cls,
+            data: dict,
+            session: AsyncSession,
+    ):
+        item = cls(**data)
+
+        session.add(item)
+        await session.commit()
+        await session.flush()
+
+        return item
