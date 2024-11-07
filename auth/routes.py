227a508b7a14a833f179f413bedec85770c94323
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-11-07 23:06:49 +0300

    Minimal user creation

--- auth/routes.py
+++ auth/routes.py
@@ -1,6 +1,11 @@
-from fastapi import APIRouter, Request
+from typing import Annotated
 
+from fastapi import APIRouter, Depends, Form, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from db import get_db
 from settings import templates
+from . import models
 
 router = APIRouter(prefix="")
 
@@ -12,3 +17,13 @@ async def sign_up(request: Request):
         name="auth/sign-up.html",
         context={},
     )
+
+
+@router.post("/api/v1/auth/sign-up")
+async def sign_up(
+        name: Annotated[str, Form()],
+        password: Annotated[str, Form()],
+        db: AsyncSession = Depends(get_db),
+):
+    user = await models.User.create({"name": name, "password": password}, db)
+    
