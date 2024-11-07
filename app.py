f1db9e6c1a003268f12775aad0d653e1307e6d40
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-11-07 23:38:00 +0300

    Include auth route

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ from starlette.staticfiles import StaticFiles
 
 from settings import MEDIA_DIR, STATIC_DIR, VERSION
 from db import init
+
+from auth.routes import router as auth_router
 from movies.routes import router as movies_router
 from videos.routes import router as videos_router
 from root.routes import router as root_router
@@ -37,6 +39,7 @@ app.mount(
     name="static",
 )
 
+app.include_router(auth_router)
 app.include_router(movies_router)
 app.include_router(root_router)
 app.include_router(videos_router)
