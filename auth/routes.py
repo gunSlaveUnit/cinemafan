fd7b6d680dc9720cbf6ef62509a8e329dc4175c7
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2024-11-07 23:37:43 +0300

    Set token to cookie

--- auth/routes.py
+++ auth/routes.py
@@ -2,10 +2,11 @@ from typing import Annotated
 
 from fastapi import APIRouter, Depends, Form, Request
 from fastapi.responses import RedirectResponse
+import jwt
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import get_db
-from settings import templates
+from settings import ALGORITHM, SECRET_KEY, templates
 from . import models
 
 router = APIRouter(prefix="")
@@ -37,6 +38,13 @@ async def sign_in_page(request: Request):
     )
 
 
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
+
+
 @router.post("/api/v1/auth/sign-in")
 async def sign_in(
         name: Annotated[str, Form()],
@@ -45,6 +53,10 @@ async def sign_in(
 ):
     user = await models.User.by_name(name, db)
     if user and user.password == password:
-        return RedirectResponse("/", status_code=303)
+        access_token = create_access_token(data={"id": str(user.id)})
+
+        response = RedirectResponse("/", status_code=303)
+        response.set_cookie(key="token", value=access_token, httponly=True)
+        return response
     else:
         print("not found")
